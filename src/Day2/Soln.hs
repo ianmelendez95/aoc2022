4f3aedf6c808e28afc96a2f3c9a4a182635a933f
Ian Melendez <imelendez@preludedynamics.com>
2022-12-01 23:23:19 -0600

    [day2] working short input

--- src/Day2/Soln.hs
+++ src/Day2/Soln.hs
@@ -9,7 +9,7 @@ import qualified Data.Text.IO as TIO
 import Data.List
 import Data.Maybe
 
-data Shape   = Rock | Paper | Scissors deriving Show
+data Shape   = Rock | Paper | Scissors deriving (Show, Eq)
 data Outcome = Win  | Lose  | Draw     deriving Show
 
 shortFile :: FilePath
@@ -25,25 +25,50 @@ solnForFile :: FilePath -> IO ()
 solnForFile file = do
   content <- TIO.readFile file
   let round_lines = T.lines content
-      rounds = map parseRoundLine round_lines
-  putStrLn $ "Rounds:  " ++ show rounds
+      points = sum $ map (resultPoints . roundResult . parseRoundLine) round_lines
+  putStrLn $ "Points:  " ++ show points
+
+resultPoints :: (Outcome, Shape) -> Int
+resultPoints (outcome, shape) = outcomePoints outcome + shapePoints shape
+  where
+    outcomePoints :: Outcome -> Int
+    outcomePoints Win  = 6
+    outcomePoints Draw = 3
+    outcomePoints Lose = 0
+
+    shapePoints :: Shape -> Int
+    shapePoints Rock     = 1
+    shapePoints Paper    = 2
+    shapePoints Scissors = 3
+
+roundResult :: (Shape, Shape) -> (Outcome, Shape)
+roundResult round@(my_shape, _) = (roundOutcome round, my_shape)
+  where
+    roundOutcome :: (Shape, Shape) -> Outcome
+    roundOutcome (their_shape, my_shape) =
+      if their_shape == my_shape
+        then Draw
+        else case their_shape of 
+              Rock     -> if my_shape == Paper    then Win else Lose
+              Paper    -> if my_shape == Scissors then Win else Lose
+              Scissors -> if my_shape == Rock     then Win else Lose
 
 parseRoundLine :: T.Text -> (Shape, Shape)
 parseRoundLine line = 
   let [their_txt, my_txt] = T.words line
    in (parseTheirShape their_txt, parseMyShape my_txt)
+  where
+    parseTheirShape :: T.Text -> Shape
+    parseTheirShape txt
+      | "A" == txt = Rock
+      | "B" == txt = Paper
+      | "C" == txt = Scissors
+      | otherwise = error $ "Unknown shape: " <> T.unpack txt
 
-parseTheirShape :: T.Text -> Shape
-parseTheirShape txt
-  | "A" == txt = Rock
-  | "B" == txt = Paper
-  | "C" == txt = Scissors
-  | otherwise = error $ "Unknown shape: " <> T.unpack txt
-
-parseMyShape :: T.Text -> Shape
-parseMyShape txt
-  | "X" == txt = Rock
-  | "Y" == txt = Paper
-  | "Z" == txt = Scissors
-  | otherwise = error $ "Unknown shape: " <> T.unpack txt
+    parseMyShape :: T.Text -> Shape
+    parseMyShape txt
+      | "X" == txt = Rock
+      | "Y" == txt = Paper
+      | "Z" == txt = Scissors
+      | otherwise = error $ "Unknown shape: " <> T.unpack txt
 
