758afcf216d61f8e05e90fdd3f185c779fdefe37
Ian Melendez <imelendez@preludedynamics.com>
2022-12-02 23:15:30 -0600

    [day3] short input working

--- src/Day3/Soln.hs
+++ src/Day3/Soln.hs
@@ -1,4 +1,3 @@
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module Day3.Soln where
@@ -9,6 +8,9 @@ import qualified Data.Text.IO as TIO
 import Data.List
 import Data.Maybe
 import Data.Char
+
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Debug.Trace
 
@@ -25,7 +27,18 @@ solnForFile :: FilePath -> IO ()
 solnForFile file = do
   content <- TIO.readFile file
   let sack_lines = T.lines content
-  undefined
+      sack_compartments = map compartments sack_lines
+      shared_items = map compartmentsSharedItem sack_compartments
+      result = sum (map (itemPriority . compartmentsSharedItem . compartments) sack_lines)
+  putStrLn $ "Answer: " <> show result
+  
+compartmentsSharedItem :: (Set Char, Set Char) -> Char
+compartmentsSharedItem (left, right) = head . Set.toList $ Set.intersection left right
+
+compartments :: T.Text -> (Set Char, Set Char)
+compartments items = 
+  let (left, right) = T.splitAt (T.length items `div` 2) items
+   in (Set.fromList (T.unpack left),  Set.fromList (T.unpack right))
 
 itemPriority :: Char -> Int
 itemPriority item
