099d1054f49dd75fd93b85a8cc15fbdf1d4e3c76
Ian Melendez <imelendez@preludedynamics.com>
2022-12-04 23:10:43 -0600

    [day4] done

--- src/Day4/Soln.hs
+++ src/Day4/Soln.hs
@@ -28,10 +28,10 @@ solnForFile file = do
   content <- TIO.readFile file
   let range_lines = T.lines content
       range_pairs = map parseRangePairs range_lines
-      answer = length (filter rangePairsContained range_pairs)
+      answer = length (filter rangePairsOverlap range_pairs)
   putStrLn "Pairs: "
-  mapM_ (\p -> putStrLn $ showPair p <> ": " <> show (rangePairsContained p)) range_pairs 
-  putStrLn $ "Answer: " <> show answer
+  mapM_ (\p -> putStrLn $ showPair p <> ": " <> show (rangePairsOverlap p)) range_pairs 
+  putStrLn $ "Answer: "   <> show answer
 
 type Range = (Int, Int)
 
@@ -40,6 +40,13 @@ showPair (left, right) = showRange left <> "," <> showRange right
   where 
     showRange :: Range -> String
     showRange (s,e) = show s <> "-" <> show e
+
+rangePairsOverlap :: (Range, Range) -> Bool
+rangePairsOverlap ((start1, end1), (start2, end2))   
+  | start1 == start2 = True  -- start at the same spot, always overlap
+  | end1   == end2   = True  -- end at the same spot, also always overlap (overcooked, undercooked)
+  | start1 < start2  = end1   >= start2
+  | otherwise        = start1 <= end2
 
 rangePairsContained :: (Range, Range) -> Bool
 rangePairsContained ((start1, end1), (start2, end2))   
