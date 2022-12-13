836d8de998a0e6b6205cd2e40e46c69aa36d3aaf
Ian Melendez <imelendez@preludedynamics.com>
2022-12-12 23:36:14 -0600

    [day13] part 1 done

--- src/Day13/Soln.hs
+++ src/Day13/Soln.hs
@@ -52,8 +52,29 @@ soln :: FilePath -> IO ()
 soln file = do
   content <- TIO.readFile file
   let input_lines = T.lines content
-      packet_pairs = parsePacketLines input_lines
-  mapM_ print packet_pairs
+      packet_pairs = zipWith (\i pair -> (i, uncurry packetsOrder pair, pair)) [1..] $ parsePacketLines input_lines
+      ordered_pairs = filter (\(_, order, _) -> order == LT || order == EQ) packet_pairs
+      ordered_indexes = map (\(i, _, _) -> i) ordered_pairs
+
+      -- equal_pairs = filter (\(_, order, _) -> order == EQ) packet_pairs
+  -- mapM_ print packet_pairs
+  -- mapM_ print equal_pairs
+  putStrLn $ "Answer: " <> show (sum ordered_indexes)
+
+packetsOrder :: Packet -> Packet -> Ordering
+packetsOrder (PInt p1)    (PInt p2)   = compare p1 p2
+packetsOrder (PList p1)   (PList p2)  = listOrder p1   p2
+packetsOrder p1@(PInt _)  (PList p2)  = listOrder [p1] p2
+packetsOrder (PList p1)   p2@(PInt _) = listOrder p1   [p2]
+
+listOrder :: [Packet] -> [Packet] -> Ordering
+listOrder [] [] = EQ
+listOrder [] _  = LT
+listOrder _ []  = GT
+listOrder (p1:ps1) (p2:ps2) = 
+  case packetsOrder p1 p2 of 
+    EQ    -> listOrder ps1 ps2
+    order -> order
 
 parsePacketLines :: [T.Text] -> [(Packet, Packet)]
 parsePacketLines [] = []
