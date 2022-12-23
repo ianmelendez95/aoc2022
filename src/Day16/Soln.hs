c5036939e5373420f8cacccf4b0b1e0f4192e45b
Ian Melendez <imelendez@preludedynamics.com>
2022-12-23 14:34:29 -0600

    [day16] reduced search space

--- src/Day16/Soln.hs
+++ src/Day16/Soln.hs
@@ -37,15 +37,24 @@ import Text.Megaparsec hiding (State (..))
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
-import Day16.FloydWarshall (Edge, floydWarshall)
+import Day16.FloydWarshall (floydWarshall)
 
 import Debug.Trace
+
+type Edge = (T.Text, T.Text)
 
 data Valve = Valve {
   valveName :: T.Text,
   valveFlow :: Int,
   valveAdj  :: [T.Text]
 } deriving Show
+
+type ValveS = State ValveE
+
+data ValveE = ValveE {
+  valvesByName :: Map T.Text Int,
+  valvesVisited :: Set T.Text
+}
 
 shortFile :: FilePath
 shortFile = "src/Day16/short-input.txt"
@@ -58,12 +67,21 @@ soln file = do
   input_lines <- T.lines <$> TIO.readFile file
   let valves = map readValveLine input_lines
       all_shortest_paths = valveShortestPaths valves
+      flowy_valve_labels = Set.fromList . map valveName . filter ((> 0) . valveFlow) $ valves
+      flowy_shortest_paths = Map.filterWithKey (filterFlowyEntry flowy_valve_labels) all_shortest_paths
   -- mapM_ print valves
-  mapM_ print (Map.toList all_shortest_paths)
+  -- mapM_ print (Map.toList all_shortest_paths)
+  putStrLn $ "Total Valve Count: " <> show (length valves)
+  putStrLn $ "Total Edges Count: " <> show (Map.size all_shortest_paths)
+  putStrLn $ "Flowing Valve Count: " <> show (length flowy_valve_labels)
+  putStrLn $ "Flowing Edges Count: " <> show (Map.size flowy_shortest_paths)
+  where 
+    filterFlowyEntry :: Set T.Text -> Edge -> Int -> Bool
+    filterFlowyEntry flowy_labels (p, q) _ = p `Set.member` flowy_labels && q `Set.member` flowy_labels
 
 valveShortestPaths :: [Valve] -> Map (T.Text, T.Text) Int
 valveShortestPaths valves = 
-  let edges_txt :: [Edge T.Text]
+  let edges_txt :: [Edge]
       edges_txt = concatMap valveEdges valves
    in floydWarshall (Map.fromList (zip edges_txt (repeat 1)))
   where 
